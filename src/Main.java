493b8d60386c78b2b3201c440149f5e80efa3404
Anton Meshkov <vigrantario@gmail.com>
2023-01-14 11:26:24 +0300

    Second commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача №1
+        int amount = 500;
+        int bonus = amount / 20;
+        System.out.println("Колличество бонусных милей: " + bonus);
+
+    }
+}
